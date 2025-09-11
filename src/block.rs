a42a0c9ccbfd37028e455d6dee4ca8430b802355
juanvasquezmarin <juanvasquez82231@elpoli.edu.co>
2025-09-11 13:42:38 -0500

    rustblockchain

---
+++ src/block.rs
@@ -0,0 +1,77 @@
+extern crate bincode;
+use bincode::{serialize,deserialize};
+use crate::{ProofOfWork, Transaction};
+use create::new_proof_of_work::ProofOfWork;
+use create::transaction::Transaction;
+use serde::{Serialize,Deserialize};
+
+pub struct Block{
+    timestamp:i64,
+    pre_block_hash:String,
+    hash:String,
+    transactions:Vec<String>,
+    nonce:u64,
+    height:usize,
+
+}
+
+impl Block{ 
+    pub fn deserialize(bytes: &[u8])->Block{
+        bincode::deserialize(bytes).unwrap()
+    }
+
+    pub fn serialize(&self)->Vec<u8>{
+        bincode::serialize(self).unwrap().to_vec()
+    }
+
+    pub fn new_block(pre_block_hash:String,transactions:&[Transaction],height:usize)->Block{
+        let mut block= Block{
+            timestamp: create::curent_timestamp(),
+            pre_block_hash,   
+            hash:String::new(),
+            transactions:trascations.to_vec(),
+            nonce: 0,
+            height, 
+        };
+        let pow = ProofOfWork::new_proof_of_work(block.clone());
+        let (nonce,hash)=pow.run();
+        block.nonce=nonce;
+        block.hash=hash;
+        return block;
+    }
+    pub fn get:transactions(&self)-> &[Transaction]{
+        self.transactions.as_slice()
+    }
+
+    pub fn get_pre_block_hash(&self)->&String{
+        self.pre_block_hash.clone()
+    }
+
+    pub fn get_hash(&self)->&str{
+        self.hash.as_str()
+    }
+    
+    pub fn get_hash_bytes(&self)->Vec<u8>{
+        self.hasg.as_bytes().to_vec()
+    }
+    pub fn get_timestamp(&self)->i64{
+        self.timestamp
+    }
+    pub fn get_height(&self)->usize{
+        self.height
+    }
+    
+    pub fn hash_transactions(&self)->String{
+        let mut txhashs=vec![];
+        for transaction in &self.transactions{
+            txhashs.extend(transaction.get_id());
+        }
+        crate::sha256_digest(txhashs.as_slice())
+    }
+    pub fn generate_genesis_block(trasactions:&Transaction)->Block{
+        let transactions=vec![transactions.clone()];
+        return Block::new_block(String::new(),&transactions,0);
+    }
+
+}
+
